3a116a1aa491887f0177336c72cb2b1774164dab
Mads Kristensen <post@madskristensen.net>
2017-02-15 12:48:06 -0800

    Cleanup

--- src/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace JavaScriptPrettier
-//{
-//    public class Constants
-//    {
-//        public static string[] FileExtensions = { ".js", ".jsx", ".es6" };
-
-//        public const string NpmPackages = "prettier@0.17.1";
-//    }
-//}
