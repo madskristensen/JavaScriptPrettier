a34f41d7a3c6302c7ca44376e850e28dec0ed8d3
Antoine Latter <aslatter@gmail.com>
2018-05-15 23:15:37 -0500

    Update Prettier to version 1.12.1 (#17)

--- src/NodeProcess.cs
+++ src/NodeProcess.cs
@@ -8,7 +8,7 @@ namespace JavaScriptPrettier
 {
     internal class NodeProcess
     {
-        public const string Packages = "prettier@1.8.2";
+        public const string Packages = "prettier@1.12.1";
 
         private static string _installDir = Path.Combine(Path.GetTempPath(), Vsix.Name, Packages.GetHashCode().ToString());
         private static string _executable = Path.Combine(_installDir, "node_modules\\.bin\\prettier.cmd");
